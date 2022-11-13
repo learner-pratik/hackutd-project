d6ab12706dd70e6a922cbf1638f8dd1fe804a73f
akkm313 <91428773+akkm313@users.noreply.github.com>
2022-11-13 03:09:31 -0600

    pdf changes

--- frontend/src/components/Plan.js
+++ frontend/src/components/Plan.js
@@ -1,19 +1,87 @@
 import React from 'react';
 import {useEffect, useState} from 'react';
-import Grid from '@mui/system/Unstable_Grid/Grid';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Box } from '@mui/system';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Pdf from "react-to-pdf";
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 
+function createData(name) {
+    return { name };
+  }
 function Plan(props) {
 
-    const subs= [{name:"DAA"},{name:"AOS"},{name:"ACN"},{name:"ALGO"},{name:"Compiler"}];
+    const ref = React.createRef();
+    const rows = [
+        createData('6363'),
+        createData('Ice cream sandwich'),
+        createData('Eclair'),
+        createData('Cupcake'),
+        createData('Gingerbread'),
+      ];
+
   return (
-  <React.Fragment>
+  <React.Fragment >
   <div className="sub-title fancy-text" >The next 2 years look good! </div>
+  <div className="table" ref={ref}>
+  <TableContainer  component={Paper}>
+      <Table   size="small" aria-label="a dense table">
+        <TableHead >
+          <TableRow>
+            <TableCell className="thead">Core Subjects</TableCell>
+          
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+
+
+    <TableContainer  component={Paper}>
+      <Table  size="small" aria-label="a dense table">
+        <TableHead >
+          <TableRow>
+            <TableCell className="thead">Elective Subjects</TableCell>
+          
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+   </div> 
+
+    <Pdf targetRef={ref} filename="code-example.pdf">
+        {({ toPdf }) => <PictureAsPdfIcon className="downloadIcon" onClick={toPdf}>Generate Pdf</PictureAsPdfIcon>}
+      </Pdf>
+
   </React.Fragment>
   )
  }
